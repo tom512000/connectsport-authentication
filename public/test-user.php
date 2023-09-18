e055721c50f3c4bfd6ddccc1cee0871ddcc933d5
Jérôme Cutrona <jerome.cutrona@univ-reims.fr>
2023-09-18 13:55:51 +0200

    feat: use AppWebPage in test-user.php

--- public/test-user.php
+++ public/test-user.php
@@ -4,37 +4,24 @@ declare(strict_types=1);
 
 use Entity\Exception\EntityNotFoundException;
 use Entity\User;
+use Html\AppWebPage;
 use Html\Helper\Dumper;
 use Html\UserProfile;
 
-$html = <<<HTML
-<!doctype html>
-<html lang="fr">
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-        <title>Test rapide des classes User et UserProfile</title>
-    </head>
-    <body>
-
-HTML;
+$p = new AppWebPage('Test rapide des classes User et UserProfile');
 
 $user1 = User::findByCredentials('essai', 'toto');
 
-$html .= Dumper::dump($user1);
+$p->appendContent(Dumper::dump($user1));
 
 $user1Profile = new UserProfile($user1);
-$html .= $user1Profile->toHTML();
+$p->appendContent($user1Profile->toHTML());
 
 try {
     User::findByCredentials('NOT-ESSAI', 'TOTO');
-    $html .= '<div style="color:red;">EntityNotFoundException?</div>';
+    $p->appendContent('<div style="color:red;">EntityNotFoundException?</div>');
 } catch (EntityNotFoundException) {
-    $html .= '<div>User `NOT-ESSAI` not found: <span style="color:green;">great!</span></div>';
+    $p->appendContent('<div>User `NOT-ESSAI` not found: <span style="color:green;">great!</span></div>');
 }
 
-$html .= <<<HTML
-    </body>
-</html>
-HTML;
-
-echo $html;
+echo $p->toHTML();
